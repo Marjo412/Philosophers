f72d4e3944653e32d61ecf187fd2a34c2015053c
Marjo412 <marjorie_4.28@hotmail.com>
2025-08-02 11:02:53 +0200

    monitor

--- src/monitor.c
+++ src/monitor.c
@@ -1,4 +1,30 @@
+
 #include "philo.h"
+
+int check_death(t_data *data)
+{
+    int     i;
+    t_philo *philo;
+    long    now;
+
+    i = 0;
+    while (i < data->nbr_philo)
+    {
+        philo = &data->philos[i];
+        pthread_mutex_lock(&data->meal_mutex);
+        now = get_time();
+        if (now - philo->last_meal_time > data->time_to_die)
+        {
+            output(philo, RED, "died\n");
+            data->end_simulation = true;
+            pthread_mutex_unlock(&data->meal_mutex);
+            return (1);
+        }
+        pthread_mutex_unlock(&data->meal_mutex);
+        i++;
+    }
+    return (0);
+}
 
 void    *monitor(void *arg)
 {
@@ -28,3 +54,10 @@ void    *monitor(void *arg)
     }
     return (NULL);
 }
+
+/*
+**check_death : this function check if the authorized survival limit time from
+    the last meal is passed, if the condition is true the philo is dead.
+**check_full: this function checks how many time the philos have eaten.
+    end_simulation if the nbr_meals are reached.
+*/
